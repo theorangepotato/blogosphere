ce9c5ecba8cc6466ec580fc2fb3def27f0bb1890
Austin Bohannon <github@alphahotelbravo.io>
2020-01-29 14:35:14 -0800

    Use file templates and add default CSS file
    * HTML is no longer generated in code, but instead very basic templates
    are used. These are available in `templates/`.
    * A default CSS file has been added to the root of the filesystem.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,57 @@
+use std::fs;
 use rss::{Channel, Item};
+use std::vec::Vec;
 
 fn main() {
-    let channel = Channel::from_url("https://xkcd.com/rss.xml").unwrap();
-    let mut page = String::from("<html>\n<head>\n<link rel=\"stylesheet\" href=\"style.css\"/>\n</head>\n<body>\n");
-    page.push_str(channel_to_html(&channel).as_str());
-    page.push_str("</body>\n</html>\n");
-
+    let mut channels = Vec::new();
+    channels.push(Channel::from_url("https://xkcd.com/rss.xml").unwrap());
+    let page = construct_page(&channels);
     print!("{}", page);
 }
 
+fn construct_page(channels: &Vec<Channel>) -> String {
+    const PAGE: &str = "{{PAGE}}";
+
+    let mut page = fs::read_to_string("templates/page.html").unwrap();
+    replace(&mut page, &PAGE, channel_to_html(&channels[0]).as_str());
+
+    page
+}
+
 fn channel_to_html(channel: &Channel) -> String {
-    let mut feed = format!("<h2>{}</h2>\n<div id=\"feed\">\n", channel.title());
+    const TITLE: &str = "{{TITLE}}";
+    const FEED: &str = "{{FEED}}";
+
+    let mut feed_file = fs::read_to_string("templates/feed.html").unwrap();
+    replace(&mut feed_file, &TITLE, channel.title());
+
+    let mut feed = String::new();
     let mut item_odd = true;
     for item in channel.items() {
         feed.push_str(print_item(&item, item_odd).as_str());
         item_odd = !item_odd;
     }
-    feed.push_str("</div>");
+    replace(&mut feed_file, &FEED, &feed.as_str());
 
-    feed
+    feed_file
 }
 
 fn print_item(item: &Item, item_odd: bool) -> String {
-        format!("<div class=\"{}\">\n\
-                <details>\n\
-                <summary><a href=\"{}\">{}</a></summary>\n\
-                <div class=\"itemContainer\">\n\
-                {}\n\
-                </div>\n\
-                </details>\n\
-                </div>\n",
-                 if item_odd {"item1"} else {"item2"},
-                 item.link().unwrap(),
-                 item.title().unwrap(),
-                 item.description().unwrap())
+    const CLASS: &str = "{{CLASS}}";
+    const LINK: &str = "{{LINK}}";
+    const TITLE: &str = "{{TITLE}}";
+    const CONTENT: &str = "{{CONTENT}}";
+
+    let mut item_file = fs::read_to_string("templates/item.html").unwrap();
+    replace(&mut item_file, &CLASS, if item_odd {"item1"} else {"item2"});
+    replace(&mut item_file, &LINK, item.link().unwrap());
+    replace(&mut item_file, &TITLE, item.title().unwrap());
+    replace(&mut item_file, &CONTENT, item.description().unwrap());
+
+    item_file
+}
+
+fn replace(source: &mut String, pattern: &str, replacement: &str) {
+    let start = source.find(&pattern).unwrap();
+    source.replace_range(start..(start + pattern.len()), replacement);
 }
