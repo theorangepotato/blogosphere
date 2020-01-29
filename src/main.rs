d80ec6505b141fe7c754eba1d90be6e6327ac173
Austin Bohannon <github@alphahotelbravo.io>
2020-01-29 14:45:28 -0800

    Only read each template file once
    * Uses lazy_static to only load each file once.
    * Criticism: Performance advantages are likely almost nil, and it may hinder
    debugging. Additionally, it adds a dependency that so far is otherwise
    unnecessary.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::fs;
 use rss::{Channel, Item};
 use std::vec::Vec;
@@ -11,8 +14,11 @@ fn main() {
 
 fn construct_page(channels: &Vec<Channel>) -> String {
     const PAGE: &str = "{{PAGE}}";
+    lazy_static! {
+        static ref PAGE_FILE: String = fs::read_to_string("templates/page.html").unwrap();
+    }
 
-    let mut page = fs::read_to_string("templates/page.html").unwrap();
+    let mut page = PAGE_FILE.clone();
     replace(&mut page, &PAGE, channel_to_html(&channels[0]).as_str());
 
     page
@@ -21,8 +27,11 @@ fn construct_page(channels: &Vec<Channel>) -> String {
 fn channel_to_html(channel: &Channel) -> String {
     const TITLE: &str = "{{TITLE}}";
     const FEED: &str = "{{FEED}}";
+    lazy_static! {
+        static ref FEED_FILE: String = fs::read_to_string("templates/feed.html").unwrap();
+    }
 
-    let mut feed_file = fs::read_to_string("templates/feed.html").unwrap();
+    let mut feed_file = FEED_FILE.clone();
     replace(&mut feed_file, &TITLE, channel.title());
 
     let mut feed = String::new();
@@ -41,8 +50,11 @@ fn print_item(item: &Item, item_odd: bool) -> String {
     const LINK: &str = "{{LINK}}";
     const TITLE: &str = "{{TITLE}}";
     const CONTENT: &str = "{{CONTENT}}";
+    lazy_static! {
+        static ref ITEM_FILE: String = fs::read_to_string("templates/item.html").unwrap();
+    }
 
-    let mut item_file = fs::read_to_string("templates/item.html").unwrap();
+    let mut item_file = ITEM_FILE.clone();
     replace(&mut item_file, &CLASS, if item_odd {"item1"} else {"item2"});
     replace(&mut item_file, &LINK, item.link().unwrap());
     replace(&mut item_file, &TITLE, item.title().unwrap());
