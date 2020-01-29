f65ef0f73ad9b5563963205149c01a2bcc365c72
Austin Bohannon <github@alphahotelbravo.io>
2020-01-29 13:38:21 -0800

    Use rss crate to parse XKCD and output simple HTML

---
+++ README.md
@@ -0,0 +1,17 @@
+# Blogosphere
+
+A simple, single user, server-side-rendered RSS feed reader for the web. This is made to be a project with minimal dependencies. It should be very easy to deploy and manage, though that will come at the cost of a lot of features some people (myself included) may find valuable.
+
+**WARNING: This project is a passion project in very alpha stages, and there is no guarantee that it works, nor that it will recieve further development.**
+
+## Install
+Currently, there is no formal installation process. You will have to build and run it yourself. To do this, see the next sections.
+
+## Build
+Just a good ol' `cargo build`. It should run on any reasonably new version of Rust.
+
+## Run
+Again, a classic `cargo run`.
+
+## License
+This project is licensed under the AGPL v3+.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rss::{Channel, Item};
+
 fn main() {
-    println!("Hello, world!");
+    let channel = Channel::from_url("https://xkcd.com/rss.xml").unwrap();
+    let mut page = String::from("<html>\n<head>\n<link rel=\"stylesheet\" href=\"style.css\"/>\n</head>\n<body>\n");
+    page.push_str(channel_to_html(&channel).as_str());
+    page.push_str("</body>\n</html>\n");
+
+    print!("{}", page);
+}
+
+fn channel_to_html(channel: &Channel) -> String {
+    let mut feed = format!("<h2>{}</h2>\n<div id=\"feed\">\n", channel.title());
+    let mut item_odd = true;
+    for item in channel.items() {
+        feed.push_str(print_item(&item, item_odd).as_str());
+        item_odd = !item_odd;
+    }
+    feed.push_str("</div>");
+
+    feed
+}
+
+fn print_item(item: &Item, item_odd: bool) -> String {
+        format!("<div class=\"{}\">\n\
+                <details>\n\
+                <summary><a href=\"{}\">{}</a></summary>\n\
+                <div class=\"itemContainer\">\n\
+                {}\n\
+                </div>\n\
+                </details>\n\
+                </div>\n",
+                 if item_odd {"item1"} else {"item2"},
+                 item.link().unwrap(),
+                 item.title().unwrap(),
+                 item.description().unwrap())
 }
